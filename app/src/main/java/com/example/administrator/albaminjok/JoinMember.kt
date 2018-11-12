eab46ff7d2369345621683e4d4f41931c0b5b134
hjshin <shj950505@naver.com>
2018-11-12 17:51:15 +0900

    happy

---
+++ app/src/main/java/com/example/administrator/albaminjok/JoinMember.kt
@@ -0,0 +1,14 @@
+package com.example.administrator.albaminjok
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+
+class JoinMember : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.join_member)
+    }
+}
+
